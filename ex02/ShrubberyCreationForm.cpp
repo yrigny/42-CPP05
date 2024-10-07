11db6fa33dc11fcad515fe8c05d03ee397e1337e
yrigny <yrigny@student.42.fr>
2024-10-07 20:02:58 +0200

    ex02 draft

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: yrigny <yrigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:54:11 by yrigny            #+#    #+#             */
-/*   Updated: 2024/10/07 19:55:15 by yrigny           ###   ########.fr       */
+/*   Updated: 2024/10/07 20:02:21 by yrigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,9 @@ void	ShrubberyCreationForm::execute(const Bureaucrat& executor) const
 		throw AForm::NotSignedException();
 	if (this->getGradeToExec() < executor.getGrade())
 		throw AForm::GradeTooLowException("Error: Executor's grade is too low to execute this form");
-	std::string	filename = this->target + "_shrubbery";
-	std::fstream		outfile(filename.c_str(), std::ios::out);
-	std::string			tree;
+	std::string		filename = this->target + "_shrubbery";
+	std::fstream	outfile(filename.c_str(), std::ios::out);
+	std::string		tree;
 	tree += "        *\n";
     tree += "       ***\n";
     tree += "      *****\n";
